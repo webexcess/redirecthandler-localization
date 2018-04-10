1f96c7c2096f370d06c8808887a2b48b737d66ca
Samuel Hauser <samuel.hauser@dotpulse.ch>
2018-04-10 11:14:32 +0200

    BUGFIX: Redirection to configured uri, if node was not found

--- Classes/Controller/RedirectController.php
+++ Classes/Controller/RedirectController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace WebExcess\RedirectHandler\Localization\Controller;
 
 use Neos\Eel\FlowQuery\FlowQuery;
@@ -11,10 +10,17 @@ use Neos\Flow\Mvc\Controller\Arguments;
 use Neos\Flow\Mvc\Controller\ControllerContext;
 use Neos\Neos\Service\LinkingService;
 use Neos\Neos\Exception;
+use Neos\Neos\Domain\Service\ContentDimensionPresetSourceInterface;
 use WebExcess\RedirectHandler\Localization\Service\LocalizationInterface;
 
 class RedirectController extends ActionController
 {
+
+    /**
+     * @Flow\InjectConfiguration(path="errorHandling")
+     * @var array
+     */
+    protected $errorHandling;
 
     /**
      * @var LinkingService
@@ -29,9 +35,17 @@ class RedirectController extends ActionController
     protected $localizationService;
 
     /**
+     * @Flow\Inject
+     * @var ContentDimensionPresetSourceInterface
+     */
+    protected $contentDimensionPresetSource;
+
+    /**
      * @param NodeInterface $node
      * @param string $identifier
      * @throws Exception
+     * @throws \Exception
+     * @throws \Neos\Eel\Exception
      * @throws \Neos\Flow\Mvc\Exception\StopActionException
      * @throws \Neos\Flow\Mvc\Exception\UnsupportedRequestTypeException
      */
@@ -41,10 +55,17 @@ class RedirectController extends ActionController
 
         $context = $this->localizationService->getTargetContext($this->request->getHttpRequest());
 
-        $targetNode = $q->context($context)->find('#' . $identifier)->get(0);
-        $uri = $this->getUriToNode($targetNode);
+        $dimensionPath = $this->getUriSegmentForDimensions($context['dimensions']);
+        $uri = '/' . $dimensionPath . $this->errorHandling['uri'];
+        $statusCode = (int)$this->errorHandling['statusCode'];
 
-        $this->redirectToUri($uri);
+        $targetNode = $q->context($context)->find('#' . $identifier)->get(0);
+        if ($targetNode !== null) {
+            $uri = $this->getUriToNode($targetNode);
+            $statusCode = 303;
+        }
+
+        $this->redirectToUri($uri, 0, $statusCode);
     }
 
     /**
@@ -72,4 +93,30 @@ class RedirectController extends ActionController
         return $uri;
     }
 
+    /**
+     * @param array $dimensionsValues
+     * @return string
+     * @throws \Exception
+     */
+    protected function getUriSegmentForDimensions(array $dimensionsValues)
+    {
+        $uriSegment = '';
+
+        foreach ($this->contentDimensionPresetSource->getAllPresets() as $dimensionName => $dimensionPresets) {
+            $preset = null;
+            if (isset($dimensionsValues[$dimensionName])) {
+                $preset = $this->contentDimensionPresetSource->findPresetByDimensionValues($dimensionName, $dimensionsValues[$dimensionName]);
+            }
+            $defaultPreset = $this->contentDimensionPresetSource->getDefaultPreset($dimensionName);
+            if ($preset === null) {
+                $preset = $defaultPreset;
+            }
+            if (!isset($preset['uriSegment'])) {
+                throw new \Exception(sprintf('No "uriSegment" configured for content dimension preset "%s" for dimension "%s". Please check the content dimension configuration in Settings.yaml', $preset['identifier'], $dimensionName), 1395824520);
+            }
+            $uriSegment .= $preset['uriSegment'] . '_';
+        }
+
+        return ltrim(trim($uriSegment, '_') . '/', '/');
+    }
 }
